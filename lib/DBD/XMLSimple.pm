1b014484038a8a3ca6aba91a3a7e851ce515cf1f
Nigel Horne <njh@bandsman.co.uk>
2016-09-03 09:08:36 -0400

    Release 0.03

--- lib/DBD/XMLSimple.pm
+++ lib/DBD/XMLSimple.pm
@@ -9,7 +9,7 @@ DBD::XMLSimple - Access XML data via the DBI interface
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
@@ -54,7 +54,7 @@ use base qw(DBI::DBD::SqlEngine);
 
 use vars qw($VERSION $drh $methods_already_installed);
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our $drh = undef;
 
 sub driver
